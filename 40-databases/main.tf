7fb855acf429c34e3afd674e601fef0d2f92b53c
akhila reddy <akhila@devops.com>
2025-11-03 20:03:47 +0530

    terraform

---
+++ 40-databases/main.tf
@@ -0,0 +1,43 @@
+resource "aws_instance" "mongodb" {
+    ami = local.ami_id
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [local.mongodb_sg_id]
+    subnet_id = local.database_subnet_id
+    
+    #user_data = file("bastion.sh")
+    tags = merge (
+        local.common_tags,
+        {
+            Name = "${local.common_name_suffix}-mongodb"
+        }
+    )
+}
+
+resource "terraform_data" "mongodb" {
+  triggers_replace = [
+    aws_instance.mongodb.id
+  ]
+  
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.mongodb.private_ip
+  }
+
+  provisioner "remote-exec" {
+        inline = [
+            "echo hello world"
+        ]
+        #     "sudo yum install -y mongodb-org",
+        #     "sudo systemctl start mongod",
+        #     "sudo systemctl enable mongod"
+        # ]
+        # connection {
+        #     type        = "ssh"
+        #     user        = "ec2-user"
+        #     private_key = file(var.ssh_private_key_path)
+        #     host        = self.public_ip
+        
+    }
+}
