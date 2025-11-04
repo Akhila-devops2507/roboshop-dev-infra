1c7a59cc57503f74e90d86e0be5441caf74ac46b
akhila reddy <akhila@devops.com>
2025-11-04 15:27:06 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -25,7 +25,7 @@ resource "terraform_data" "mongodb" {
     host     = aws_instance.mongodb.private_ip
   }
 
-  #terraform copies this file to mongodb server
+  # terraform copies this file to mongodb server
   provisioner "file" {
     source = "bootstrap.sh"
     destination = "/tmp/bootstrap.sh"
@@ -33,7 +33,7 @@ resource "terraform_data" "mongodb" {
 
   provisioner "remote-exec" {
     inline = [
-      "chmod +x /tmp/bootstrap.sh",
+        "chmod +x /tmp/bootstrap.sh",
         # "sudo sh /tmp/bootstrap.sh"
         "sudo sh /tmp/bootstrap.sh mongodb"
     ]
@@ -121,3 +121,84 @@ resource "terraform_data" "rabbitmq" {
   }
 }
 
+# resource "aws_instance" "mysql" {
+#     ami = local.ami_id
+#     instance_type = "t3.micro"
+#     vpc_security_group_ids = [local.mysql_sg_id]
+#     subnet_id = local.database_subnet_id
+#     iam_instance_profile = aws_iam_instance_profile.mysql.name
+    
+#     tags = merge (
+#         local.common_tags,
+#         {
+#             Name = "${local.common_name_suffix}-mysql" # roboshop-dev-mysql
+#         }
+#     )
+# }
+
+# resource "aws_iam_instance_profile" "mysql" {
+#   name = "mysql"
+#   role = "EC2SSMParameterRead"
+# }
+
+# resource "terraform_data" "mysql" {
+#   triggers_replace = [
+#     aws_instance.mysql.id
+#   ]
+  
+#   connection {
+#     type     = "ssh"
+#     user     = "ec2-user"
+#     password = "DevOps321"
+#     host     = aws_instance.mysql.private_ip
+#   }
+
+#   # terraform copies this file to mongodb server
+#   provisioner "file" {
+#     source = "bootstrap.sh"
+#     destination = "/tmp/bootstrap.sh"
+#   }
+
+#   provisioner "remote-exec" {
+#     inline = [
+#         "chmod +x /tmp/bootstrap.sh",
+#         "sudo sh /tmp/bootstrap.sh mysql dev"
+#     ]
+#   }
+# }
+
+# resource "aws_route53_record" "mongodb" {
+#   zone_id = var.zone_id
+#   name    = "mongodb-${var.environment}.${var.domain_name}" # mongodb-dev.daws86s.fun
+#   type    = "A"
+#   ttl     = 1
+#   records = [aws_instance.mongodb.private_ip]
+#   allow_overwrite = true
+# }
+
+# resource "aws_route53_record" "redis" {
+#   zone_id = var.zone_id
+#   name    = "redis-${var.environment}.${var.domain_name}" # redis-dev.daws86s.fun
+#   type    = "A"
+#   ttl     = 1
+#   records = [aws_instance.redis.private_ip]
+#   allow_overwrite = true
+# }
+
+# resource "aws_route53_record" "mysql" {
+#   zone_id = var.zone_id
+#   name    = "mysql-${var.environment}.${var.domain_name}" # mysql-dev.daws86s.fun
+#   type    = "A"
+#   ttl     = 1
+#   records = [aws_instance.mysql.private_ip]
+#   allow_overwrite = true
+# }
+
+# resource "aws_route53_record" "rabbitmq" {
+#   zone_id = var.zone_id
+#   name    = "rabbitmq-${var.environment}.${var.domain_name}" # rabbitmq-dev.daws86s.fun
+#   type    = "A"
+#   ttl     = 1
+#   records = [aws_instance.rabbitmq.private_ip]
+#   allow_overwrite = true
+# }
