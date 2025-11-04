c7f7081d000a3f9d945a5fae2b0328dee95fd1cf
akhila reddy <akhila@devops.com>
2025-11-04 11:04:17 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
 dnf install ansible -y
-ansible-pull -U https://github.com/Akhila-devops2507/ansible-roboshop-roles-tf.git -e component=mongodb main.yaml
+
+# Pull and run the Ansible playbook for MongoDB
+ansible-pull -U https://github.com/Akhila-devops2507/ansible-roboshop-roles-tf.git -e role_name=mongodb main.yaml
