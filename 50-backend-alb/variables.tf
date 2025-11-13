0ba38f7d77e3b685eb6f8c4c6066decdd3931b34
akhila reddy <akhila@devops.com>
2025-11-13 20:55:48 +0530

    terraform

--- 50-backend-alb/variables.tf
+++ 50-backend-alb/variables.tf
@@ -5,3 +5,11 @@ variable "project_name" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+    default = "Z05335291MAWEUN326PVQ"
+}
+
+variable "domain_name" {
+    default = "akhiladevops.fun"
+}
