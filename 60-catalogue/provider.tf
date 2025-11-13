8676660ed4211e10903bad2d2124d062488e029e
akhila reddy <akhila@devops.com>
2025-11-13 20:43:18 +0530

    terraform

--- 60-catalogue/provider.tf
+++ 60-catalogue/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "remote-state-86s-dev"
+    bucket = "state-dev-remote"
     key    = "roboshop-dev-catalogue"
     region = "us-east-1"
     use_lockfile = true
